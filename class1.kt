ce8245a05aca422a1bc9e0a1c6472c5c5a2faa1d
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-08-14 13:59:11 +0530

    class 1

---
+++ class1.kt
@@ -0,0 +1,44 @@
+
+fun main() {
+    print("hello tarun")
+    println("Hello, world!!!")
+    // comments in kotlin
+
+    // This is a single line comment
+    /* this is a multi line comment 
+     * this is a multi line comment package.
+     * .
+     * .
+     *                                  */
+     
+     
+}
+
+////////////////////////////////////////////////////////////////////
+// var in kotlin 
+fun main(args:Array<String>){
+    var a=40
+    print(a)
+    var tarun=40
+    print(tarun)
+}
+
+////////////////////////////////////////////////////////////////////
+// using `` in variable bec try is a keyword
+
+fun main(args:Array<String>){
+    var a=40
+    print(a)
+    var `try`=40
+    print(`try`)
+}
+
+////////////////////////////////////////////////////////////////////
+// giving error bec a is int but now changing to double so it will give error
+fun main(args:Array<String>){
+    var a=40
+    a=40.5
+    print(a)
+    var Try=40
+    print(Try)
+}
