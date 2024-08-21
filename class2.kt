ced7df28c3e6f04fb0794c10387c2dbac49dfb8f
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-08-21 13:42:51 +0530

    class 2

---
+++ class2.kt
@@ -0,0 +1,34 @@
+fun main(args:Array<String>){
+    // variables in kotlin
+//     var a=40
+//     var b=40
+//     println(a+b)
+//     val c=20;
+//     val d=20;
+//     print(c+d);
+
+// this will give error because it is not intilized
+// var f
+
+// var a: Int
+//     a=10
+//     val b=30
+//     var c:Int
+//     c=a+b
+//     print(c)
+    
+    // var a:Int
+    // a=10
+    // var b:Int
+    // b=20
+  // var ____c=40
+  // print(a+b+c)
+  // var string =40
+  // print(a+b+String)
+    // var str ing =40
+    // print(a+b+str ing)
+    // var 123tarun=50
+    // print(a+b+123tarun)
+    var age:Int =35
+    println(age);
+}
