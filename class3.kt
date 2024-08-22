ca02e188cd14f531db9670d534ef1b85af48c569
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-08-22 13:21:06 +0530

    class3

---
+++ class3.kt
@@ -0,0 +1,19 @@
+fun main(){
+//     using char datatype in kotlin
+    
+    val name:Char='A'
+    println(name)
+    
+//     using string datatype in kotlin
+//     mutiple line string
+    var fullName:String="""my name is tarun choudhary
+    i am from Palampur himachal pradesh
+    """  
+    println(fullName)
+    
+//     using boolean datatype in kotlin
+
+    val name2:Boolean;
+    name2=true;
+    println(name2)
+}
