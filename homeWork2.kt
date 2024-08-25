7363b552c5374c0163a83fa9075a6da64378763f
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-08-25 12:47:49 +0530

    homework2

---
+++ homeWork2.kt
@@ -0,0 +1,14 @@
+fun main() {
+//define two variables
+    // define a variable that tells that the user has a membership or not
+    
+val user="Tarun choudhary"
+    val MemberOrNot:Boolean=true;
+    if(MemberOrNot){
+        println("User is a member")
+        
+    }
+    else{
+        print("user is not a member")
+    }
+}
