2bef10f1e0d4953246da80e50d6c75dd8ce00720
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-08-25 12:21:32 +0530

    discount program

---
+++ homeWork.kt
@@ -0,0 +1,21 @@
+fun main() {
+//     write a java program for discount system 
+//    var name="Tarun"
+    val age=60;
+   val gender="female";
+    val finalAmount:Int
+    val totalAmount=10000
+    
+    if(age>60 && age <120){
+    finalAmount=totalAmount-totalAmount * 10 / 100
+        println("Final ammount: $finalAmount")
+    }
+    else if(gender=="female"){
+            finalAmount=totalAmount-totalAmount * 5 / 100
+        println("Final amount: $finalAmount")
+    }
+    else{
+       println("Final amount: $totalAmount")
+    }
+    
+}
