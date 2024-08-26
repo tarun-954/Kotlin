beffacaa15d071f21bef541a10d171604e293ed1
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-08-26 13:28:57 +0530

    class4

--- class4.kt
+++ class4.kt
@@ -7,9 +7,15 @@ fun main() {
     
     
     // and operater 
-    var x=true
-    var y=false
-    println(x&&y)
-    println(x.and(y))
-    
+    var x1=true
+    var y1=false
+    println(x1&&y1)
+    println(x1.and(y1))
+    var x=2
+    println(+x)
+    println(-x)
+    println(x++) //increment to 3 but not printed
+    println(++x) // incremented and also printed
+    println(x--) // decremented but not printed
+        println(--x) // decremented and also printed
 }
