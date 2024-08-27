15b1ca913b92659d015956288c73e6cbec485d3e
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-08-27 13:59:54 +0530

    class5

--- class5.kt
+++ class5.kt
@@ -1,4 +1,9 @@
 //default function with int return type
+
+class car{
+    var make:String=""
+    var make:String=""
+}
 fun anytask():Int{
    var a=32;
     var b=24;
@@ -11,15 +16,38 @@ fun task2(a:Int,b:Int):Int{
     val c=a+b;
     return c;
 }
+
+ //recursion function
+    var count =0;
+    fun rec(){
+        count++;
+        if(count<=5){
+            println("hello "+count);
+            rec();
+        }
+    }
 fun main() {
 //     calling function 
    val d=anytask();
     
 //     calling function
     val f=task2(20,30);
+    
+  
     println(d)
     println(f)
-    
-    
+   rec()
+   
+   
+   
+   
+   
+   
+   //h.w
+  
+   // factorial using recursion
+    //sum of n numbers using recursion
+    // find sum of even numbers for the given range of 1 to n using reccursion and function calling
+//make a class and create two diiferent object of this cllass and pass differnet parametere to the class
     
 }
